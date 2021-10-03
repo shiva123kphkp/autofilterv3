f91c4f787896beb34d84d346285cd5dda87bd773
shiva123kphkp <91113459+shiva123kphkp@users.noreply.github.com>
2021-10-03 14:35:40 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🚀  JOIN  🚀', url=https://t.me/mallumoviesp)
+                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
